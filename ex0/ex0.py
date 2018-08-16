3db640919cbf2988f6d1038bdc88bc4aad94aeb3
firelf-no1 <35374256+firelf-no1@users.noreply.github.com>
2018-08-16 12:11:37 +0300

    Update ex0.py

--- ex0/ex0.py
+++ ex0/ex0.py
@@ -1,6 +1,6 @@
 ##############################################################
 # FILE : ex0.py
-# WRITER : itai shopen , firelf , 021982038
+# WRITER : itai shopen
 # EXERCISE : intro2cs ex0 2017-2018
 # DESCRIPTION: A simple program that prints "Hello World!" to
 # the standard output (screen).
