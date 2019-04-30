bdbf10d1201080431fae4bd595c282a4d18b6560
itai shopen <35374256+itaishopen@users.noreply.github.com>
2019-04-30 13:47:37 +0300

    Update and rename ex3/ex3.py to ex03/ex3.py

--- ex03/ex3.py
+++ ex03/ex3.py
@@ -1,6 +1,6 @@
 ##############################################################
 # FILE : ex3.py
-# WRITER : itai shopen , firelf , 021982038
+# WRITER : itai shopen
 # EXERCISE : intro2cs ex3 2017-2018
 # DESCRIPTION: loops
 #############################################################
